a75cfadef62fd91617a7d1f02c1392e9cb2a30b4
FabbriSimo01 <fabbri200985@gmail.com>
2022-12-28 14:56:59 +0100

    n

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 define("UPLOAD_DIR", "./db/upload/");
+require_once("utils/functions.php");
 require_once("db/database.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "RATATWEET", 3306);
 ?>
