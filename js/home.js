7c0f9d78b47c62d95deb6dfbc3e30fd9506d2edc
mario-biavati <mario.biavati@studio.unibo.it>
2023-01-20 11:59:33 +0100

    changed home style and added params

--- js/home.js
+++ js/home.js
@@ -15,31 +15,31 @@ function printPost(idPost) {
             let post = r.data;
             let ratingRounded = Math.round(post.avgRating);
             let htmlContent = 
-            `<article class="d-flex flex-column flex-md-row">
+            `<article class="ms-2 me-2 me-md-0 d-flex flex-column flex-md-row">
                 <div class="d-flex d-md-none">
                     <div>
-                        <h1>NOME_POST</h1>
-                        <span>NOME_UTENTE</span>
+                        <h1>${post.title}</h1>
+                        <span>${post.username}</span>
                     </div>
-                    <div class="d-flex flex-fill justify-content-end"><img src="img/recipe-icon-save.png" style="width: 40px; height: 40px;"/></div>
+                    <a href="post.php?id=${post.IDpost}" class="d-flex flex-fill justify-content-end"><img src="img/recipe-icon.png" style="width: 40px; height: 40px;"/></a>
                 </div>
-                <a href="#" class="col-md-4 me-2">
-                    <img src="test/food-placeholder.png" class="img-fluid col-12 me-2" style="object-fit: cover;"/>
+                <a href="post.php?id=${post.IDpost}" class="col-md-4 me-2">
+                    <img src="data:image/png;base64,${post.pic}" class="img-fluid col-12 me-2" style="object-fit: cover;"/>
                 </a>
                 <div class="d-flex flex-column">
                     <div class="d-flex d-none d-md-block flex-column">
                         <div class="d-flex flex-row">
                             <div class="d-flex flex-column">
-                                <h1>NOME_POST</h1>
-                                <span>NOME_UTENTE</span>
+                                <h1>${post.title}</h1>
+                                <span>${post.username}</span>
                             </div>
-                            <div class="d-flex flex-fill justify-content-end"><img src="img/recipe-icon-save.png" style="width: 40px; height: 40px;"/></div>
+                            <a href="post.php?id=${post.IDpost}" class="d-flex flex-fill justify-content-end"><img src="img/recipe-icon.png" style="width: 40px; height: 40px;"/></a>
                         </div>
-                        <p class="mt-4" style="max-height: 150px; overflow:hidden;"> a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a</p>
+                        <p class="mt-4" style="max-height: 150px; overflow:hidden;">${post.description}</p>
                     </div>
                     <div class="d-flex flex-fill rating">
                         <div class="d-flex flex-fill justify-content-end mt-1"><img src="img/comment-icon.png" style="width: 40px; height: 40px;"/></div>
-                        <span class="ratingScore">4.5</span>`
+                        <span class="ratingScore">${post.avgRating}</span>`
             for (let i = 5; i > 0; i--) {
                     htmlContent += `<span`;
                     if (ratingRounded == i) {
@@ -51,7 +51,8 @@ function printPost(idPost) {
             htmlContent += `
                     </div>
                 </div>
-            </article>`;
+            </article>
+            <hr/>`;
             main.innerHTML += htmlContent;
             resolve(htmlContent);
         });
