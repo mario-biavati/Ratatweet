3de2dcc7691db5b90e525510967b0600387d5f61
Eritropene <michele.ravaioli3@studio.unibo.it>
2023-01-23 09:57:56 +0100

    modify home.js post.js

--- js/post.js
+++ js/post.js
@@ -64,13 +64,13 @@ function printReply(idComment, where, first = false) {
         let comment = r.data;
         let htmlContent = 
         `<div id="${comment.IDcomment}" class="mt-2 pb-2 border-bottom">
-        <div class="d-flex col-1" style="max-width: 60px; min-width: 50px;">
-            <img src="${comment.pic}" style="max-width: 40px; max-height: 40px; margin-top: 10px;">
-        </div>
         <div class="d-flex flex-column col-10 col-lg-11">
-            <a class="fw-bold" href="user_page.php?id=${comment.IDuser}">
-                ${comment.username}
-            </a>
+            <div class="d-flex justify-content-start">
+                <img src="data:image/png;base64,${comment.pic}" class="icon-small">
+                <a class="fw-bold my-auto ms-3" href="user_page.php?id=${comment.IDuser}">
+                    ${comment.username}
+                </a>
+            </div>
             <p>
                 ${comment.text}
             </p>
